9783bbd73e77e9dae3271b64d5432e7b433e9671
coherentchaos <scott.axcell@gmail.com>
2019-01-26 20:36:58 -0700

    fix k regular graph creation

--- src/main/java/cs455/node/Overlay.java
+++ src/main/java/cs455/node/Overlay.java
@@ -3,6 +3,7 @@ package cs455.node;
 import cs455.util.Utils;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Overlay {
     private int cr;
@@ -12,6 +13,8 @@ public class Overlay {
 
     private Overlay(Set<String> nodes, int cr) {
         this.nodes.addAll(nodes);
+        nodes.stream()
+            .forEach(n -> connections.computeIfAbsent(n, l -> new ArrayList<>()));
         this.cr = cr;
     }
 
@@ -23,8 +26,7 @@ public class Overlay {
         if (!isConnectionRequirementValid(nodes.size(), cr))
             return false;
 
-        constructLinearTopology();
-        connectNodesRandomly();
+        createKRegularGraph();
 
         if (!checkNumberOfLinks())
             return false;
@@ -36,32 +38,105 @@ public class Overlay {
         return true;
     }
 
-    private void connectNodesRandomly() {
-        for (String node : nodes)
-            connectNode(node);
+    private void dumpOverlay() {
+        Utils.out("NODES:\n");
+        for (String node : nodes) {
+            Utils.out(node + "\n");
+        }
+        Utils.out("\nCONNECTIONS:\n");
+        for (Map.Entry<String, List<String>> entry : connections.entrySet()) {
+            Utils.out(entry.getKey()+"\n");
+            List<String> sinks = entry.getValue();
+            Collections.sort(sinks);
+            for (String sink : sinks) {
+                Utils.out("  " + sink +"\n");
+            }
+        }
+        Utils.out("\nLINKS:\n");
+        List<String> printMe = new ArrayList<>();
+        for (Link link : links) {
+            printMe.add(link.getSource() + " -> " + link.getSink() + "\n");
+        }
+        Collections.sort(printMe);
+        for (String s : printMe) {
+            Utils.out(s);
+        }
     }
 
-    private void constructLinearTopology() {
+    private void linkNodeXStepsAway(int index, int steps) {
+        int neighborIndex = getNeighborIndexXStepsAway(index, steps);
+        String source = nodes.get(index);
+        String sink = nodes.get(neighborIndex);
+        addLink(Link.of(source, sink));
+    }
+
+    private int getNeighborIndexXStepsAway(int index, int steps) {
+        int neighborIndex = index;
+        for (int step = 0; step < steps; step++) {
+            neighborIndex++;
+            if (neighborIndex == nodes.size())
+                neighborIndex = 0;
+        }
+        return neighborIndex;
+    }
+
+    private void createKRegularGraph() {
+        Collections.shuffle(nodes);
+
+        if (cr == 1) {
+            constructLinearTopology();
+            for (Link link : links)
+                addSink(link.getSource(), link.getSink());
+            return;
+        }
+
+        if (isEven(cr)) {
+            // If k=2m is even, put all the vertices around a circle, and join
+            // each to its m nearest neighbors on either side
+            int m = cr / 2;
+            for (int i = 0; i < nodes.size(); i++) {
+                for (int j = 1; j <= m; j++)
+                    linkNodeXStepsAway(i, j);
+            }
+        }
+        else {
+            // If k=2m+1 is odd, and n is even, put the vertices on a circle,
+            // join each to its m nearest neighbors on each side, and also to the vertex directly opposite
+            int m = (cr - 1) / 2;
+            for (int i = 0; i < nodes.size(); i++) {
+                for (int j = 1; j <= m; j++)
+                    linkNodeXStepsAway(i, j);
+                linkNodeXStepsAway(i, nodes.size() / 2);
+            }
+        }
+
+        for (Link link : links) {
+            addSink(link.getSource(), link.getSink());
+            addSink(link.getSink(), link.getSource());
+        }
+    }
+
+    private boolean isEven(int i) {
+        return i % 2 == 0;
+    }
+
+     private void constructLinearTopology() {
         for (int i = 0; i < nodes.size(); i++) {
             String source = nodes.get(i);
             if (i == nodes.size() - 1) {
-                // last node connects to first node
                 String sink = nodes.get(0);
-                connections.computeIfAbsent(source, l -> new ArrayList<>()).add(sink);
-                links.add(Link.of(source, sink));
+                addLink(Link.of(source, sink));
             }
             else {
-                // connect to next node
                 String sink = nodes.get(i + 1);
-                connections.computeIfAbsent(source, l -> new ArrayList<>()).add(sink);
-                links.add(Link.of(source, sink));
+                addLink(Link.of(source, sink));
             }
         }
     }
 
     private boolean isConnectionRequirementValid(int n, int k) {
         // k-regular graph conditions
-        // n = k+1
+        // n >= k+1
         // nk is even
         if ((n < (k + 1)) || (((n * k) % 2) != 0)) {
             Utils.error("overlay is not valid. k-regular graph conditions not met.");
@@ -81,33 +156,31 @@ public class Overlay {
         return true;
     }
 
-    private void connectNode(String source) {
-        // already created a linear topology so decrement by 1
-        int requiredNumConnections = cr - 1;
-        int numConnections = 0;
-        for (int i = 1; i <= requiredNumConnections; i++) {
-            if (numConnections >= requiredNumConnections)
-                return;
+        private void addLink(Link link) {
+        if (!links.contains(link))
+            links.add(link);
+    }
 
-            // get random connection, if same node, try again
-            int nodeIndex = nodes.indexOf(source);
-            while (nodeIndex == nodes.indexOf(source))
-                nodeIndex = getRandomNodeIndex();
+    private void addSink(String source, String sink) {
+        if (!connections.get(source).contains(sink))
+            connections.get(source).add(sink);
+    }
 
-            // check connection doesn't exist other way round
-            String sink = nodes.get(nodeIndex);
-            if (connections.containsKey(sink)) {
-                if (connections.get(sink).contains(source)) {
-                    numConnections++;
-                    connections.computeIfAbsent(source, l -> new ArrayList<>()).add(sink);
-                    continue;
-                }
-            }
+    private int getNumberOfSinks(String source) {
+        return connections.get(source).size();
+    }
 
-            connections.computeIfAbsent(source, l -> new ArrayList<>()).add(sink);
-            links.add(Link.of(source, sink));
-            numConnections++;
-        }
+    private List<String> getValidSinks(String source) {
+        return connections.entrySet().stream()
+            .filter(e -> !e.getKey().equals(source))
+            .filter(e -> e.getValue().size() < cr)
+            .filter(e -> !getConnectedSinks(source).contains(e.getKey()))
+            .map(e -> e.getKey())
+            .collect(Collectors.toList());
+    }
+
+    private List<String> getConnectedSinks(String source) {
+        return connections.get(source);
     }
 
     private boolean checkNumberOfLinks() {
@@ -141,16 +214,11 @@ public class Overlay {
         return dfs.isSinkReachable(source, sink);
     }
 
-    private int getRandomNodeIndex() {
-        return new Random().nextInt(nodes.size());
+    private int getRandomNodeIndex(int maxInt) {
+        return new Random().nextInt(maxInt);
     }
 
     public String[] getNodeConnections(String node) {
-//        List<String> nodes = new ArrayList<>();
-//        for (Link link : links) {
-//            if (link.getSource().equals(node))
-//                nodes.add()
-//        }
         return links.stream()
             .filter(l -> l.getSource().equals(node))
             .map(l -> l.getSink())
@@ -178,5 +246,15 @@ public class Overlay {
             }
             return false;
         }
+    }
+
+     @Override
+    public String toString() {
+        return "Overlay{" +
+            "cr=" + cr +
+            ", nodes=" + nodes +
+            ", connections=" + connections +
+            ", links=" + links +
+            '}';
     }
 }
