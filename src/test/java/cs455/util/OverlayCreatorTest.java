9bf981704976adcf839908de2118b810ca00fc5d
coherentchaos <scott.axcell@gmail.com>
2019-01-27 17:54:07 -0700

    add junit 4 to gradle so it builds and runs outside IDEA

---
+++ src/test/java/cs455/util/OverlayCreatorTest.java
@@ -0,0 +1,24 @@
+package cs455.util;
+
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class OverlayCreatorTest {
+
+    @Test
+    public void testSetup() {
+        String[] nodes = new String[]{"host0:0", "host1:1", "host2:2", "host3:3", "host4:4",
+            "host5:5", "host6:6", "host7:7", "host8:8", "host9:9"};
+
+        for (int i = 0; i < 10000; i++) {
+            for (int j = 1; j < 10; j++) {
+                OverlayCreator overlayCreator = OverlayCreator.of(Stream.of(nodes).collect(Collectors.toSet()), j);
+                assertTrue(overlayCreator.setup());
+            }
+        }
+    }
+}
